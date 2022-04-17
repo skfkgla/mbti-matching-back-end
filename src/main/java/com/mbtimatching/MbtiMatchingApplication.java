d83e16039b1a005c3b2d3f33588e11b61665e249
skfkgla <narahim.lee@gmail.com>
2022-04-17 17:22:46 +0900

    first commit

---
+++ src/main/java/com/mbtimatching/MbtiMatchingApplication.java
@@ -0,0 +1,13 @@
+package com.mbtimatching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbtiMatchingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbtiMatchingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mbtimatching/MbtiMatchingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbtimatching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbtiMatchingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
