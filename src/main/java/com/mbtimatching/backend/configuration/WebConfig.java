5f6a772c0fb14d81f28d73e1b4380d43706dcc96
skfkgla <narahim.lee@gmail.com>
2022-04-19 23:08:51 +0900

    cors설정 설명 추가

--- src/main/java/com/mbtimatching/backend/configuration/WebConfig.java
+++ src/main/java/com/mbtimatching/backend/configuration/WebConfig.java
@@ -10,6 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOriginPatterns("*") //cors 문제 해결
-                .allowCredentials(true);
+                .allowCredentials(true);    //originPatterns를 와일드카드로 지정하고 credentials를 true로 주면 안됨(도메인 잡아줄것)
+                                            //개발 블로그 참고
     }
 }
