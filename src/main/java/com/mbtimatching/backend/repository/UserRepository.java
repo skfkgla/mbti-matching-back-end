c5e14b2daaffae1f730b1ba7ff79e6be0fa9aa81
skfkgla <narahim.lee@gmail.com>
2022-05-17 21:48:15 +0900

    [feat] 사용자 회원가입 구현

---
+++ src/main/java/com/mbtimatching/backend/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.mbtimatching.backend.repository;
+
+import com.mbtimatching.backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUserId(String userId);
+}
