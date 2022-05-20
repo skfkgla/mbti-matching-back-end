5ad56450169600fd95b2f5511e1aa1cdcda14c94
skfkgla <narahim.lee@gmail.com>
2022-05-20 16:10:23 +0900

    [feat] 로그인 테스트코드 작성

--- src/test/java/com/mbtimatching/backend/provider/service/UserServiceTests.java
+++ src/test/java/com/mbtimatching/backend/provider/service/UserServiceTests.java
@@ -3,8 +3,10 @@ package com.mbtimatching.backend.provider.service;
 
 import com.mbtimatching.backend.core.type.MbtiType;
 import com.mbtimatching.backend.entity.User;
+import com.mbtimatching.backend.exception.error.LoginFailedException;
 import com.mbtimatching.backend.repository.UserRepository;
 import com.mbtimatching.backend.web.dto.RequestUser;
+import com.mbtimatching.backend.web.dto.ResponseUser;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,4 +43,31 @@ public class UserServiceTests {
 
         assertNotNull(userRepository.findByUserId("test"));
     }
+    @Test
+    @DisplayName("회원가입 서비스 테스트")
+    @Transactional
+    void loginTest(){
+        //회원 정보 등록
+        RequestUser.Register user = RequestUser.Register.builder()
+                .userId("test")
+                .password("test")
+                .gender("man")
+                .mbti(MbtiType.ENFJ)
+                .birth(new Date())
+                .nickname("닉네임")
+                .build();
+        userService.register(user);
+        //로그인
+        RequestUser.Login loginDto = RequestUser.Login.builder()
+                .userId("test")
+                .password("test")
+                .build();
+        //token이 잘 발급 되는지 확인
+        ResponseUser.Login loginResponse = userService.login(loginDto).orElseGet(()-> null);
+        System.out.println(loginResponse.getAccessToken());
+        System.out.println(loginResponse.getRefreshToken());
+        assertNotNull(loginResponse);
+    }
+
+
 }
